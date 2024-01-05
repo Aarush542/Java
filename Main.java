67123c8c98d07bc6c08882704667ee8971737a59
Aarush542 <100684799+Aarush542@users.noreply.github.com>
2024-01-05 19:10:50 +0530

    First commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+// 56+78+34+12
+public class Main{
+    public static void main(String[] args)
+    {
+        int a=56;
+        int b=78;
+        int c=34;
+        int d=12;
+        int e;
+        e=a+b+c+d;
+        System.out.println(e);
+    }
+}
