6591e8ad95da95c685b020661fc70aa7d379353a
Aarush542 <100684799+Aarush542@users.noreply.github.com>
2024-01-08 02:03:52 +0530

    Second commit

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
-// 56+78+34+12
+// number swap :
+// without third variable 
+
+import java.util.*;
+
 public class Main{
-    public static void main(String[] args)
-    {
-        int a=56;
-        int b=78;
-        int c=34;
-        int d=12;
-        int e;
-        e=a+b+c+d;
-        System.out.println(e);
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        System.out.println("Values of a is "+a);
+        System.out.println("Values of b is "+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("Swap values of a " +a);
+        System.out.println("Swap values of b " +b);
     }
 }
