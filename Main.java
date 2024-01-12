4effe815943cafd697c49aec6b400f579b4cb5bd
Aarush542 <100684799+Aarush542@users.noreply.github.com>
2024-01-12 19:21:02 +0530

    Fourth commit

--- Main.java
+++ Main.java
@@ -1,32 +1,16 @@
-// // Question 8: Ticket Booking System
-// // Design a ticket booking system for a cinema that calculates the ticket price based on the age of the customer. 
-// Children (age < 12) get a 50% discount, 
-// while seniors (age >= 65) get a 30% discount.
+// neon number 
+// 9 ka square 
+// 81 => 8+1=(9) 
 
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args) {
-        Scanner obj=new Scanner(System.in);
-        System.out.println("Enter age for ticket discount");
-        int age=obj.nextInt();
-        int price=300;
-        System.out.println("Ticket Price"+price);
-        
-        if(age<12)
-        {
-            int discount=price-(price*50/100);
-            System.out.println("Ticket Price for Children after Discount: "+discount);
+public class Main {
 
-        }
-        else if(age>=65)
-        {
-            int discount=price-(price*30/100);
-            System.out.println("Ticket Price for Senior Citizen after Discount: "+discount);
-        }
-        else
-        {
-            System.out.println("Ticket price for Adult: "+price);
-        }
+    public static void main(String[] args) {
+     Scanner obj=new Scanner(System.in);
+     System.out.println("Enter number");
+     int a =obj.nextInt();
+     int n=a*a;
+        System.out.println(n);
     }
 }
