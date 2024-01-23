ececf8d7aaf2c88d3c462befa445c96c272ac6aa
Aarush542 <100684799+Aarush542@users.noreply.github.com>
2024-01-23 15:11:36 +0530

    Sixth commit

--- Main.java
+++ Main.java
@@ -1,75 +1,47 @@
-// 5)
-// A class 
-// constructor  
-// show1
-// show2
+// 3)
+// Problem Statement – Goutam and Tanul plays by telling numbers. Goutam says a number
+// to Tanul. Tanul should first reverse the number and check if it is same as the original. If yes,
+// Tanul should say “Palindrome”. If not, he should say “Not a Palindrome”. If the number is
+// negative, print “Invalid Input”. Help Tanul by writing a program.
+// Sample Input 1 :
+// 21212
+// Sample Output 1 :
+// Palindrome
+// Sample Input 2 :
+// 6186
 
-// B class 
-// constructor  
-// show3
-// show4
+// Sample Output 2 :
+// Not a Palindrome
 
-// Main class 
-// constructor
-// show5
-// show6
+import java.util.Scanner;
 
-// main() :access  
-class A{
-    A()
-    {
-System.out.println("constructor");
-    }
-    void show1()
-    {
-System.out.println("show1");
-    }
-    void show2()
-    {
-System.out.println("show2");
-    }
-}
-class B{
-    B()
-    {
-       System.out.println("constructor"); 
-    }
-    void show3()
-    {
-System.out.println("show3");
-    }
-    void show4()
-    {
-      System.out.println("show4");  
-    }
-}
-class C{
-    C()
-    {
-      System.out.println("constructor");  
-    }
-    void show5()
-    {
-System.out.println("show5");
-    }
-    void show6()
-    {
-        System.out.println("show6");
-    }
-}
 public class Main {
 
     public static void main(String[] args) {
-        A obj=new A();
-        obj.show1();
-        obj.show2();
-
-        B obj1=new B();
-        obj1.show3();
-        obj1.show4();
-
-        C obj2=new C();
-        obj2.show5();
-        obj2.show6();
+        Scanner obj =new Scanner(System.in);
+        System.out.println("Enter value to check for pallindrome");
+        int a=obj.nextInt();
+        int rem,sum=0;
+        int temp=a;
+        if(a<=0)
+        {
+            System.out.println("Invalid Input");
+        }
+        else{
+        while(a>0)
+        {
+            rem=a%10;
+            sum=sum*10+rem;
+            a=a/10;
+        }
+        if(temp==sum)
+        {
+            System.out.println("Tanul : Pallindrome");
+        }
+        
+        else{
+            System.out.println("Tanul : Not Pallindrome");
+        }
+    } 
     }
 }
