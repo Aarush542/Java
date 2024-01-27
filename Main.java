b037f49db6bbba97fdf5332939476db4d1961220
Aarush542 <100684799+Aarush542@users.noreply.github.com>
2024-01-28 00:59:13 +0530

    Seventh commit

--- Main.java
+++ Main.java
@@ -1,47 +1,110 @@
-// 3)
-// Problem Statement – Goutam and Tanul plays by telling numbers. Goutam says a number
-// to Tanul. Tanul should first reverse the number and check if it is same as the original. If yes,
-// Tanul should say “Palindrome”. If not, he should say “Not a Palindrome”. If the number is
-// negative, print “Invalid Input”. Help Tanul by writing a program.
-// Sample Input 1 :
-// 21212
-// Sample Output 1 :
-// Palindrome
-// Sample Input 2 :
-// 6186
+// 5)
+// class A 
+// constructor :default :this is a constructor
+// add method:a+b :no return and no argument 
+// sub method:a-b  :no return and  argument 
 
-// Sample Output 2 :
-// Not a Palindrome
+// class B 
+// constructor :parameter :a*b 
+// mod method:a%b : return and no argument 
+// multi method:a*b  : return and argument 
+
+
+// Main class 
+// Main constructor :parameter type 
+// sub method:a-b  :no return and  argument 
+
+
+// main method :
+// A 
+// B 
+// Main class 
 
 import java.util.Scanner;
 
-public class Main {
+class A{
+   A show()
+   {
+      System.out.println("This is a constructor");
+      return this;
+   }
+   void add()
+   {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter a for addition");
+      int a=sc.nextInt();
+      System.out.println("Enter b for addition");
+      int b=sc.nextInt();
+      System.out.println(a+b);
 
-    public static void main(String[] args) {
-        Scanner obj =new Scanner(System.in);
-        System.out.println("Enter value to check for pallindrome");
-        int a=obj.nextInt();
-        int rem,sum=0;
-        int temp=a;
-        if(a<=0)
-        {
-            System.out.println("Invalid Input");
-        }
-        else{
-        while(a>0)
-        {
-            rem=a%10;
-            sum=sum*10+rem;
-            a=a/10;
-        }
-        if(temp==sum)
-        {
-            System.out.println("Tanul : Pallindrome");
-        }
-        
-        else{
-            System.out.println("Tanul : Not Pallindrome");
-        }
-    } 
+   }
+   void sub(int a,int b)
+   {
+      System.out.println(a-b);
+      
+   }
+}
+class B{
+   B(int c,int d)
+   {
+System.out.println(c*d);
+   }
+int mod()
+{
+   Scanner sc =new Scanner(System.in);
+   System.out.println("Enter 1st value for mod");
+   int a=sc.nextInt();
+   System.out.println("Enter 1st value for mod");
+   int b=sc.nextInt();
+return a%b;
+}
+
+int multi(int e, int f)
+{
+return e*f;
+} 
+}
+public class Main {
+    void a(int g, int h)
+    {
+      System.out.println(g-h);
     }
+   public static void main(String[] args) {
+      A k=new A();
+      k.show();
+      k.add();
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter a for subtraction");
+      int a=sc.nextInt();
+      System.out.println("Enter b for subtraction");
+      int b=sc.nextInt();
+      k.sub(a, b);
+      System.out.println("Enter 1st value for Multi");
+      int c=sc.nextInt();
+      System.out.println("Enter 2nd value for Multi");
+      int d=sc.nextInt();
+      
+
+      B k1=new B(c, d);
+      System.out.println(k1.mod());
+      System.out.println("Enter 1st number for multiplication");
+      int e=sc.nextInt();
+      System.out.println("Enter 2nd number for multiplication");
+      int f=sc.nextInt();
+      System.out.println(k1.multi(e, f));
+      System.out.println("Enter number");
+      sc.nextInt();
+
+      Main k2=new Main();
+      System.out.println("Enter g for subtraction");
+      int g=sc.nextInt();
+      System.out.println("Enter h for subtraction");
+      int h=sc.nextInt();
+      k2.a(g, h);
+      
+
+      
+      
+   
+   }
 }
