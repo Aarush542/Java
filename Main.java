daefa2c4351ce4b6d6824809a8601b6cde78e2d8
Aarush542 <100684799+Aarush542@users.noreply.github.com>
2024-02-06 13:27:34 +0530

    Tenth commit

--- Main.java
+++ Main.java
@@ -1,55 +1,83 @@
-// A ex B
-// show1 :A class 
+// abstract class A 
+// abstract method add() :no return and no argument 
+// abstract method sub():no return and  argument
+// abstract method multi(): return and no argument
+// abstract method div(): return and  argument
+// abstract method modular(): return and  argument
 
-// B ex C
-// show2 : B class
 
-// C ex D
-// show 3 :C class 
 
-// D 
-// show 4:D class 
+// class Main ex A
 
-// Main
-// one child k through all properties access
-class A extends B{
-   void show1()
-   {
-      
-      System.out.println("A class");
-   }
+// main method access 
+
+import java.util.Scanner;
+
+abstract class A{
+
+abstract void add();
+abstract void sub();
+abstract void multi();
+abstract void div();
+abstract void mod();
 }
 
-class B extends C{
+public class Main extends A{
+   void add()
+   {
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter 1st number for add");
+   int a=sc.nextInt();
+   System.out.println("Enter 2nd number for add");
+   int b=sc.nextInt();
+   System.out.println(a+b);
+   }
    
-   void show2()
+   void sub()
    {
-      
-      System.out.println("B class");
+      Scanner sc=new Scanner(System.in);
+   System.out.println("Enter 1st number for Sub");
+   int a=sc.nextInt();
+   System.out.println("Enter 2nd number for Sub");
+   int b=sc.nextInt();
+   System.out.println(a-b);
    }
-}
-
-class C extends D{
    
-   void show3()
+   void multi()
    {
-      System.out.println("C class");
+      Scanner sc=new Scanner(System.in);
+   System.out.println("Enter 1st number for multi");
+   int a=sc.nextInt();
+   System.out.println("Enter 2nd number for multi");
+   int b=sc.nextInt();
+   System.out.println(a*b);
    }
-}
-
-class D{
-   void show4()
+   
+   void div()
    {
-      System.out.println("D class");
+      Scanner sc=new Scanner(System.in);
+   System.out.println("Enter 1st number for div");
+   int a=sc.nextInt();
+   System.out.println("Enter 2nd number for div");
+   int b=sc.nextInt();
+   System.out.println(a/b);
    }
-}
-public class Main {
-
+   
+   void mod()
+   {
+      Scanner sc=new Scanner(System.in);
+   System.out.println("Enter 1st number for Mod");
+   int a=sc.nextInt();
+   System.out.println("Enter 2nd number for Mod");
+   int b=sc.nextInt();
+   System.out.println(a%b);
+   }
    public static void main(String[] args) {
-      A k=new A();
-      k.show1();
-      k.show2();
-      k.show3();
-      k.show4();
+    Main k =new Main();
+    k.add();
+    k.sub();
+    k.multi();
+    k.div();
+    k.mod();  
    }
 }
